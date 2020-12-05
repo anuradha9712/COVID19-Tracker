b8f1f7e211befc48994df3ff3e345a4b0a3c85ac
anuradha9712 <anuradha.mcs18.du@gmail.com>
2020-12-05 20:36:08 +0530

    update design
    Signed-off-by: anuradha9712 <anuradha.mcs18.du@gmail.com>

--- src/components/HereMap.js
+++ src/components/HereMap.js
@@ -7,7 +7,7 @@ const Map =()=>{
             <br/> <br/>
            <center> 
                <iframe title="Covid19-cases" width="560" height="560" src="https://app.developer.here.com/coronavirus/" frameBorder="0"></iframe>
-               <iframe title="Covid19-cases" width="560" height="560" src="https://app.developer.here.com/covid19-healthcare-resources/" frameBorder="0"></iframe>
+               {/*<iframe title="Covid19-cases" width="560" height="560" src="https://app.developer.here.com/covid19-healthcare-resources/" frameBorder="0"></iframe>*/}
 
             </center>   
         </div>
